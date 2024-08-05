5b8125fd7a5be2f186bc512617283c417fb02c05
MR. Subi <bantawasubin@gmail.com>
2024-08-05 10:32:35 +0545

    user modelSchema

--- server/model/userModeel.js
+++ server/model/userModeel.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 3, maxlength: 30 },
-  email: {
-    trype: String,
-    required: true,
-    minlength: 3,
-    maxlength: 200,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 30 },
+    email: {
+      trype: String,
+      required: true,
+      minlength: 3,
+      maxlength: 200,
+      unique: true,
+    },
+    password: { trype: String, required: true, minlength: 6, maxlength: 100 },
   },
-  password: { trype: String, required: true, minlength: 6, maxlength: 100 },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-    timestamps: true
-});
+const useModel = mongoose.model("User", userSchema);
