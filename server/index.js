790946ea48364a60e57d1e1b295050a866c23326
MR. Subi <bantawasubin@gmail.com>
2024-08-05 10:08:13 +0545

    chat app

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config(); 
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URL;
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+
+mongoose.connect(uri)
+.then(() => console.log("MongoDB connection established!"))
+.catch((error) => console.log("MongoDB connection failed", error.message));
