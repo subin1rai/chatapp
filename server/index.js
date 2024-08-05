90b4a3b0961200ca5c3fd8407d1d0e0b359df6dc
MR. Subi <bantawasubin@gmail.com>
2024-08-05 10:16:58 +0545

    mongoose & route  boilerplate

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req,res)=>{
+    res.send("welcome ")
+})
+
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URL;
 
